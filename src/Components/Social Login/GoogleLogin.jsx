f61a855b37bdeb589232d00b3ba573564bff3878
MD Jinnah Akondo <mdjinnahakondo@gmail.com>
2025-12-06 21:55:23 +0600

    useAxios Secure  hook adde but there is a little problem

--- src/Components/Social Login/GoogleLogin.jsx
+++ src/Components/Social Login/GoogleLogin.jsx
@@ -3,15 +3,26 @@ import { FcGoogle } from 'react-icons/fc';
 import useAuth from '../../Hooks/useAuth';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
 
 const GoogleLogin = () => {
     const { googleLogin } = useAuth();
+
+    const axiosSecure = useAxiosSecure();
 
     const navigate = useNavigate()
 
     const handelLogin = () => {
         googleLogin()
-            .then(() => {
+            .then(async (res) => {
+                const user = {
+                    name: res?.user?.displayName,
+                    email: res?.user?.email,
+                    role: 'user',
+                    profileImage: res?.user?.photoURL,
+                    createdAt: new Date()
+                }
+                await axiosSecure.post('/users', user)
                 navigate('/');
                 toast.success('loged in successfull')
             })
---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import axios from 'axios';
+const instance = axios.create({
+    baseURL: 'http://localhost:3000'
+})
+const useAxiosSecure = () => {
+    const { user } = useAuth();
+    useEffect(() => {
+        // intercept request 
+        const reqInterceptor = instance.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`
+            return config;
+        })
+
+        //intercept response
+        // const resInterceptor = instance.interceptors.response.use(response => {
+        //     return response;
+        // }, error => {
+        //     console.log(error);
+        // }
+        // )
+        return () => {
+            instance.interceptors.request.eject(reqInterceptor);
+            // instance.interceptors.response.eject(resInterceptor);
+        }
+    }, [user])
+
+    return instance;
+};
+
+export default useAxiosSecure;
